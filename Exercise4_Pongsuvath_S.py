ce3e31e02c95ded16ca386863e9d7d126d9adf69
Tong69 <115133315+Tong69@users.noreply.github.com>
2022-10-06 12:45:35 +0700

    Create Exercise4_Pongsuvath_S.py

---
+++ Exercise4_Pongsuvath_S.py
@@ -0,0 +1,11 @@
+Foundation_English = 86
+General_Business = 77
+Introduction_to_Computer_systems = 88
+Computer_Programming = 65
+
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Introduction_to_Computer_systems)
+print("Computer Programming :",Computer_Programming)
+print("-------------------")
